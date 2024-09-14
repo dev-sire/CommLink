b48eaf578e017050ae7ccffc4fb2b828bf230f97
Aman Shahid <shahidamaan2019@gmail.com>
2024-09-14 23:20:29 +0500

    added initial auth configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
 import AuthScreen from "@/features/auth/components/auth-screens";
 
 export default function Home() {
   return(
-    <div>
       <AuthScreen />
-    </div>
   )
 }
--- src/features/auth/components/auth-screens.tsx
+++ src/features/auth/components/auth-screens.tsx
@@ -2,13 +2,21 @@
 
 import { useState } from "react"
 import { SignInFlow } from "../type"
+import { SignInCard } from "./sign-in-card"
+import { SignUpCard } from "./sign-up-card"
 
 const AuthScreen = () => {
     const [state, setState] = useState<SignInFlow>("signIn")
 
   return (
-    <div className="h-full flex items-center justify-center bg-[#5c3b58]">
-        AuthScreen
+    <div className="flex h-full items-center justify-center bg-[#5C3B58]">
+      <div className="md:h-auto md:w-[420px]">
+        {state === 'signIn' ? (
+          <SignInCard setState={setState} />
+        ) : (
+          <SignUpCard setState={setState} />
+        )}
+      </div>
     </div>
   )
 }
