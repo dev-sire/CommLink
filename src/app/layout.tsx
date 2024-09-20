9034004eaf221fd3a2363cd2caabfe370542a5a9
Aman Shahid <shahidamaan2019@gmail.com>
2024-09-20 17:55:37 +0500

    added dynamic toutes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import { ConvexClientProvider } from "@/components/convex-client-provider";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import { Modals } from "@/components/modals";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           className={inter.className}
         >
           <ConvexClientProvider>
+            <Toaster />
             <Modals />
             {children}
           </ConvexClientProvider>
--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -2,50 +2,56 @@ import {
     Dialog,
     DialogContent,
     DialogHeader,
-    DialogDescription,
     DialogTitle
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
 import { useCreateWorkspace } from "../api/use-create-workspace";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export const CreateWorkspaceModal = () => {
-    const [open, setOpen] = useCreateWorkspaceModal()
-    const { mutate } = useCreateWorkspace()
+    const router = useRouter()
+    const[open, setOpen] = useCreateWorkspaceModal()
+    const[name, setName] = useState("")
+    const{ mutate, isPending } = useCreateWorkspace()
 
     const handleClose = () => {
         setOpen(false)
-        // TODO: Clear form content
+        setName("")
     }
 
-    const handleSubmit = () => {
-        mutate({
-            name: "Workspace 1",
-        }, {
-            onSuccess(data) {
-                // use router to navigate to the newly created workspace...
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        mutate({ name }, {
+            onSuccess(id){
+                toast.success("Workspace Created!")
+                router.push(`/workspace/${id}`)
+                handleClose();
             },
         })
     }
-
     return (
         <Dialog open={open} onOpenChange={handleClose}>
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Add a workspace</DialogTitle>
                 </DialogHeader>
-                <form className="space-y-4">
+                <form onSubmit={handleSubmit} className="space-y-4">
                     <Input 
-                        value=""
-                        disabled={false}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        disabled={isPending}
                         minLength={3}
                         autoFocus
                         placeholder="Workspace name e.g 'name', 'work', 'personal'"
                         required
                     />
                     <div className="flex justify-end">
-                        <Button disabled={false}>
+                        <Button disabled={isPending}>
                             Create
                         </Button>
                     </div>
