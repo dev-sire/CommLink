260863373464d45886c726b30d4b5a5d157f4d85
Aman Shahid <shahidamaan2019@gmail.com>
2024-09-21 22:51:35 +0500

    added sidebar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,15 +1,7 @@
-'use client'
-
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 const WorkspaceIdPage = () => {
-    const workspaceId = useWorkspaceId()
-    const{ data } = useGetWorkspace({ id: workspaceId })
-
     return(
         <div>
-            Workspace Data: {JSON.stringify(data)}
+            Workspace ID Page
         </div>
     )
 }
